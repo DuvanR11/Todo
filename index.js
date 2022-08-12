b2872dba2d4aff7db05b6b56771fe7e9ec6e0b2f
DuvanR11 <durivera@gmail.com>
2022-08-11 22:22:23 -0500

    despligue base

---
+++ index.js
@@ -0,0 +1,56 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector("ul");
+const empty = document.querySelector(".empty");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text !== "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    li.appendChild(tachar())
+    ul.appendChild(li);
+
+    input.value = "";
+    empty.style.display = "none";
+  }
+});
+
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    console.log(item)
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteBtn;
+}
+function tachar() {
+    const tacharBtn = document.createElement("button");
+    tacharBtn.textContent = "O";
+    tacharBtn.className = "btn-delete";
+
+    tacharBtn.addEventListener("click", (e) => {
+        const item = e.target.parentElement.childNodes[0];
+        item.style.textDecoration = 'line-through'
+    })
+    return tacharBtn
+}
